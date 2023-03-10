23c38cae48e75ab0dfd7e47a593928e729225717
shyam <shyamkorade0@gmail.com>
2023-03-11 00:00:18 +0530

    my first code

---
+++ 1first.c
@@ -0,0 +1,6 @@
+# include <stdio.h>
+
+int main(){
+    printf("Hello Shyam!. Welcome, glad to see you again...");
+    return 0;
+}
---
+++ 2variable.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a = 4;
+    int b = 3;
+    float c = 8.5;
+
+    printf("The addition of a and b %d\n", a +b);
+    printf("Addition of b and c is %f", b+c);
+    return 0 ;
+}
