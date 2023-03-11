020b0d2c4a88eb83ddb20f8beeb764ded7f954b5
shyam <shyamkorade0@gmail.com>
2023-03-11 07:56:56 +0530

    program for addition of two numbers

---
+++ 01_Practice/01_practice.c
@@ -0,0 +1,17 @@
+
+// Addition of two numbers using user output
+#include <stdio.h>
+
+int main(){
+    int length, breadth;
+
+    printf("What is the length of the rectangle\n");
+    scanf("%d", &length);
+
+    printf("What is the breadth of the rectangle\n");
+    scanf("%d", &breadth);
+
+    printf("Area of rectangle is %d", length * breadth);
+
+     return 0;
+}
