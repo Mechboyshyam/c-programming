4e67d5cfe32dc6b99c6ab0f9b68497065ca91b86
shyam <shyamkorade0@gmail.com>
2023-03-11 08:06:08 +0530

    Program for finding area of circle

--- 01_Practice/02_practice.c
+++ 01_Practice/02_practice.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-
+// To find the area of circle by user input...
 int main(){
-    
+    int radius;
+    float pie = 3.142;
+
+    printf("What is the radius of circle\n");
+    scanf("%d",&radius);
+
+    printf("The area of circle is %f", pie*radius*radius);
      return 0;
 }
