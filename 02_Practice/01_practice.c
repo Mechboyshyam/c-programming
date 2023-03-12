4e8ca59f69c99428e423d5e85ab21659437fc593
shyam <shyamkorade0@gmail.com>
2023-03-12 09:15:34 +0530

    divisibility test program

--- 02_Practice/01_practice.c
+++ 02_Practice/01_practice.c
@@ -3,7 +3,13 @@
 
 int main(){
     int a = 3^3;
-
     printf("The value of a is %d",a);
+
+    //int b = 3^3; -->This is xor operation and it is valid but it will not give any value
+
+    char b= 'shyam';
+
+   // char contain only one character not allows multiple character 
+
      return 0;
 }
---
+++ 02_Practice/02_practice.c
@@ -0,0 +1,13 @@
+// To check whether the num is divisible or not?
+
+#include <stdio.h>
+
+int main(){
+    int num;
+
+    printf("Enter the value \n");
+    scanf("%d",&num);
+
+    printf("Divisibility test returns : %d", num%97);
+     return 0;
+}
