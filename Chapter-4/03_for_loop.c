4960bc33b1fb592fdce750f55aec0eda1ea5d698
shyam <shyamkorade0@gmail.com>
2023-03-15 21:00:14 +0530

    for loop syntax with one program

---
+++ Chapter-4/03_for_loop.c
@@ -0,0 +1,14 @@
+// Code for FOR loop 
+// syntax of for loop
+// for (define the function; condition; increment or decrement)
+
+#include <stdio.h>
+
+int main(){
+    for (int a; a<10; a++)
+    {
+        printf("%d is the value which comes under a loop\n",a+1);
+    }
+    
+     return 0;
+}
