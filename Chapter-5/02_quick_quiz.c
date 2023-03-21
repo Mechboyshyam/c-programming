9a53d9ca37bd2eaa9a9a4e8b2cb6fa57a2a9f0cd
shyam <shyamkorade0@gmail.com>
2023-03-21 21:05:00 +0530

    quick quize

---
+++ Chapter-5/02_quick_quiz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+ 
+void Goodmorning();
+void Goodafternoon();
+void Goodnight();
+
+int main(){
+    Goodmorning();
+    Goodafternoon();
+    Goodnight();
+     return 0;
+}
+
+void Goodmorning(){
+    printf("Good morning Shyam\n");
+}
+
+void Goodafternoon(){
+    printf("Good afternoon Shyam\n");
+}
+
+void Goodnight(){
+    printf("Good night Shyam\n");
+}
