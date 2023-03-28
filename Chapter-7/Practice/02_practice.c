c080aacf6de7a44d99cb20d57112ceb5e6444c18
shyam <shyamkorade0@gmail.com>
2023-03-28 07:13:12 +0530

    program for finding table

---
+++ Chapter-7/Practice/02_practice.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int multiplication[10];
+    for(int i = 0; i<10; i++){
+        multiplication[i] = 5 * (i+1);
+    }
+    
+    for(int i = 0; i<10; i++){
+        printf("5 X %d = %d\n",i+1,multiplication[i]);
+    }
+     return 0;
+}
